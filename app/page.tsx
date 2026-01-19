beef7684765ed7cc3e5b56ced6b4f471116d8716
Abigail Hong <psyah14@nottingham.ac.uk>
2026-01-19 14:24:33 +0000

    Squashed commit of the following:
    commit f6c8370b3f588613152a7f3a06edac4f80e30188
    Author: Abigail Hong <psyah14@nottingham.ac.uk>
    Date:   Mon Jan 19 14:19:55 2026 +0000
    
        Implement 'Start' button as a link
    
    commit a3d59eebd103b7cbc3f9418a2ec922e4be7ff034
    Author: Abigail Hong <psyah14@nottingham.ac.uk>
    Date:   Thu Jan 15 19:02:20 2026 +0000
    
        Add titles, buttons, and icons
    
    commit 72c7277d498e090b018830a131fba1dd59bb66d0
    Author: Abigail Hong <psyah14@nottingham.ac.uk>
    Date:   Thu Dec 4 17:28:34 2025 +0000
    
        Create basic project structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,46 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import Link from "next/link";
+import { useState } from "react";
+
+export default function HomePage() {
+  const [isOn, setIsOn] = useState(false);
+  const handleToggle = () => setIsOn(!isOn);
+
+  const toggleButtonClasses = `w-auto px-8 py-4 rounded-full text-2xl font-bold shadow-md transition mt-4 ${
+    isOn ? "bg-teal-400 text-white" : "bg-gray-300 text-zinc-800"
+  }`;
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex flex-col items-center justify-start min-h-screen px-4">
+      <div className="flex flex-col text-center mt-8 space-y-2">
+        <h1 className="main-title">
+          SCAM SLAM
+        </h1>
+
+        <p className="text-6xl mt-2 mb-2">
+          SEE HOW YOU RANK IN SPOTTING SCAMS
+        </p>
+
+        <div className="flex flex-col items-center mt-8 space-y-6">
+          <button
+            onClick={handleToggle}
+            className={toggleButtonClasses}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            {isOn ? "DIGITAL BUZZERS ON" : "DIGITAL BUZZERS OFF"}
+          </button>
+
+          <Link
+            href="/enter-name"
+            className=" bg-green-400 hover:bg-green-300 text-zinc-800 font-semibold px-16 py-8 rounded-full text-6xl shadow-md transition mt-6">
+              START ROUND
+          </Link>
         </div>
-      </main>
-    </div>
+      </div>
+
+      <footer className="mt-4 flex justify-center py-4 mb-10">
+        <img src="images/icons/Icons(b).svg" alt="Icons" className="h-20 w-auto" />
+      </footer>
+    </main>
   );
 }
